1ba7c619bb2314cd6fd546a51a2b99295e1cfd7e
Yared <yagidaobit@gmail.com>
2025-03-19 21:24:50 +0300

    working with is_lower string method

---
+++ is_lower.py
@@ -0,0 +1,5 @@
+name = 'i am me'     # i assigned a string which is called i am me in a variable called name
+
+result = name.islower()    # i used islower() method to check the strings in a variable called name is in lower case or not 
+
+print(result)   # i print to check and the expected result is true
