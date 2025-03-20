d628a446cc0983a9a3367da2a9d1b93b14fce5f5
Yared <yagidaobit@gmail.com>
2025-03-20 19:48:25 +0300

    capitalize.py

---
+++ capitalize.py
@@ -0,0 +1,5 @@
+name = 'my name is yared'
+
+result = name.capitalize() # i used this string method to make my first letter in uppercase.
+
+print(result)   # the expected result is My name is yared
