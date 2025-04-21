9dea0badf6a5d0788b325479e06f3bc72c916639
YagiDao12 <yagidaobit@gmail.com>
2025-04-21 10:52:00 +0300

    list methods

---
+++ list_methods/append.py
@@ -0,0 +1,5 @@
+name = ['yared', 'solomon', 'abebe']
+
+name.append('john')
+
+print(name)
---
+++ list_methods/copy.py
@@ -0,0 +1,8 @@
+name = ['yared', 'solomon', 'abebe']
+
+copy_name = name.copy()
+
+
+copy_name.remove('yared')
+
+print(copy_name)
