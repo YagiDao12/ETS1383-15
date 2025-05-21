4767d1c11db658ef81316893c4e6528f4280ecca
YagiDao12 <yagidaobit@gmail.com>
2025-05-21 14:10:12 +0300

    set operations

---
+++ set_operation/add.py
@@ -0,0 +1,7 @@
+# Creating a set
+my_set = {1, 2, 3}
+
+# Adding an element to the set
+my_set.add(4)
+
+print(my_set)  # Output: {1, 2, 3, 4}
---
+++ set_operation/remove.py
@@ -0,0 +1,7 @@
+# Creating a set
+my_set = {10, 20, 30}
+
+# Removing an element from the set
+my_set.remove(20)
+
+print(my_set)  # Output: {10, 30}
